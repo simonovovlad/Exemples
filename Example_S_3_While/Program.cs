cb20823eeb1afb345f114b07da770480546d1a46
simonovovlad <simonovovlad@gmail.com>
2022-09-18 03:36:35 +0300

    add Example S 3 While

---
+++ Example_S_3_While/Program.cs
@@ -0,0 +1,8 @@
+﻿int time = 11;
+while(time < 22)
+{
+time++;
+Console.WriteLine("You can watch the cartoons. time: " + time);
+}
+Console.WriteLine("Time to sleep");
+Console.WriteLine("It is 22 o'clock", time);
