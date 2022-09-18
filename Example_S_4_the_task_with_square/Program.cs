7ade2373e22a72b4f485d16a2f6d827c452cef9a
simonovovlad <simonovovlad@gmail.com>
2022-09-18 05:05:08 +0300

    add Example S 4 the task with square

---
+++ Example_S_4_the_task_with_square/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите квадрат: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = b * b;
+if(c == a)
+{
+Console.Write("Верный коррень! Молодец!");
+}
+else
+{
+Console.Write("Не Верный коррень!");
+}
