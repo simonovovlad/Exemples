e117c6c7a6841fea251c136972905f1307d143a4
simonovovlad <simonovovlad@gmail.com>
2022-09-20 01:40:50 +0300

    add Example S 5_days of week

---
+++ Example_S_5_days_of_week/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Enter the number of the day of the week from 1 to 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("Sunday");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Monday");
+}
+else if (day == 3)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (day == 4)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (day == 5)
+{
+    Console.WriteLine("Thursday");
+}
+else if (day == 6)
+{
+    Console.WriteLine("Friday");
+}
+else if (day == 7)
+{
+    Console.WriteLine("Saturday");
+}
+else
+{
+   Console.WriteLine("Wrong enter"); 
+}
+
+
