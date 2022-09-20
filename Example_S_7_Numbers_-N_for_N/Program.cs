25d2f66eaca6f6c9bea3e1da34c9cf9fdb33220e
simonovovlad <simonovovlad@gmail.com>
2022-09-20 03:36:48 +0300

    add Example S 7 Numbers -N for N

---
+++ Example_S_7_Numbers_-N_for_N/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу каторая на вход принимает одно число (N),
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Enter the number ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+int num2 = -num;
+while (num2 <= num)
+{
+    Console.Write(num2 + ", ");
+    num2 = num2 + 1;
+}
+
