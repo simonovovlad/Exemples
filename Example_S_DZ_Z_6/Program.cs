63eec1d11494bf336bcddfe64dcdda0943eb505e
simonovovlad <simonovovlad@gmail.com>
2022-09-22 16:39:57 +0300

    fix Example_S_DZ_Z_6

--- Example_S_DZ_Z_6/Program.cs
+++ Example_S_DZ_Z_6/Program.cs
@@ -6,7 +6,7 @@
 // 7 -> нет
 
 Console.WriteLine("Введите число : ");
-int a = Convert.ToInt32(Console.Read());
+int a = Convert.ToInt32(Console.ReadLine());
 if (a % 2 == 0)
 Console.Write("Четное");
 else 
