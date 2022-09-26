d75d1b99cb880940b60946fbc654be1068ea45d3
simonovovlad <simonovovlad@gmail.com>
2022-09-26 16:55:34 +0300

    add Example_S2_Void, Example_S_DZ_Z_10_13_15

---
+++ Example_S2_Void/Program.cs
@@ -0,0 +1,69 @@
+﻿void Z_9()
+{
+    // Random randomov = new Random();
+    // int number = randomov.Next(10, 100);
+    Console.WriteLine("Enter the number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int tens = number / 10;
+    int ones = number % 10;
+
+    Console.WriteLine(tens);
+    Console.WriteLine(ones);
+
+    int max = tens;
+    int min = tens;
+
+    if (ones > max) max = ones;
+    if (ones < max) min = ones;
+    if (Convert.ToBoolean(max == min)) Console.WriteLine("Числа равны");
+    else
+    {
+    Console.Write("Большее: ");
+    Console.WriteLine(max);
+    Console.Write("Меньшее: ");
+    Console.WriteLine(min);
+    }
+}
+//Z_9();
+
+void Z_11()
+{
+    int randomov = new Random().Next(100, 1000);
+    // int number1 = randomov / 100;
+    // int number2 = randomov % 10;
+    // String number3 = Convert.ToString(number1) + Convert.ToString(number2);
+    // Console.WriteLine(randomov);
+    // Console.WriteLine(number3);
+    int randomov1 = randomov / 100;
+    int randomov2 = randomov / 10 % 10;
+    int randomov3 = randomov % 10;
+    Console.WriteLine(randomov);
+    Console.WriteLine ("" + randomov1 + "," + randomov3);
+}
+//Z_11();
+
+void Z_12()
+// Напишите программу, которая будет принимать
+// на вход два числа и выводить, является ли второе
+// число кратным первому. Если число 2 не кратно числу
+// 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+{
+Console.WriteLine("Введите делимое: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите делитель: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = number1 % number2;
+if (number1 % number2 == 0)
+    {
+    Console.WriteLine(number1 + " - кратно " + number2);
+    }
+else
+    {
+    Console.WriteLine(number1 + " - не кратно " + number2);
+    Console.WriteLine(number3 + " - остаток ");
+    }
+
+}
+Z_12();
---
+++ Example_S_DZ_Z_10_13_15/Program.cs
@@ -0,0 +1,63 @@
+﻿void Z_10()
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+{
+    Console.WriteLine("Enter the number: ");
+    int randomov = Convert.ToInt32(Console.ReadLine());
+    //int randomov = new Random().Next(100, 1000);
+    int randomov2 = randomov / 10 % 10;
+    Console.WriteLine(randomov);
+    Console.WriteLine (randomov2);
+}
+Z_10();
+void Z_13()
+{
+// Задача 13: Напишите программу, которая с помощью деления выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 3267432979 -> 6
+
+// Console.WriteLine("Enter the number: ");
+// int randomov = Convert.ToInt32(Console.ReadLine());
+// int randomov3 = randomov % 10;
+
+Console.Write("Введи число: ");
+int randomov = Convert.ToInt32(Console.ReadLine());
+string randomov3 = Convert.ToString(randomov);
+if (randomov3.Length > 2)
+{
+  Console.WriteLine("third - " + randomov3[2]);
+}
+else 
+{
+  Console.WriteLine("- There is no third number");
+}
+}
+//Z_13();
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void Z_15 () 
+{
+  Console.WriteLine("Введите номер деня недели: ");
+  int day = Convert.ToInt32(Console.ReadLine());
+  
+  if (day == 6 || day == 7) 
+  {
+  Console.WriteLine("это выходной");
+  }
+  else if (day < 1 || day > 7) 
+  {
+    Console.WriteLine("не является днем недели");
+  }
+  else Console.WriteLine("не выходной");
+}
+//Z_15 ();
