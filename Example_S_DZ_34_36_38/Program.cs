a15e132e093a55c08a73c7150254293de2588baf
simonovovlad <simonovovlad@gmail.com>
2022-10-06 16:38:04 +0300

    add Example_S_DZ_34_36_38

---
+++ Example_S_DZ_34_36_38/Program.cs
@@ -0,0 +1,136 @@
+﻿void Z34() //Задача 34:Рандом на количество чётных чисел в массиве.
+{
+/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+Console.WriteLine("Enter the size of the array");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int count = 0;
+random(numbers);
+print(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    count++;
+}
+
+
+Console.WriteLine($"the number of even numbers in the array -> {count} ");
+
+void random(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+void print(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+}
+Z34();
+void Z36()//Найдите сумму элементов, стоящих на нечётных позициях.
+{
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 
+*/
+
+int size = 4;
+int[] numbers = new int[size];
+random(numbers);
+print(numbers);
+
+int sum = 0;
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sum += numbers[i];
+}
+Console.Write(sum);
+
+
+
+void random(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+}
+//Z36();
+void Z38()//Найдите разницу между максимальным и минимальным элементами массива.
+{
+/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементами массива.
+[3 7 22 2 78] -> 76
+*/
+
+int size = 10;
+int[] numbers = new int[size];
+random(numbers);
+print(numbers);
+
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"Min: {min}");
+Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Diff Min and Max: {max-min}");
+
+
+
+void random(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 555);
+    }
+}
+
+
+void print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+}
+//Z38();
